5d194551b064dd5144bf1d96d24b475a8b7b45cf
Nick Verdin <nv1028@aggienetwork.com>
2020-03-17 18:30:47 -0500

    finished mvp

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
+import axios from 'axios';
+import uuid from 'uuid';
 
-const Friends = () => {
+const Friends = (props) => {
     const [friendsList, setFriendsList] = useState([]);
+    const [newFriend, setNewFriend] = useState({
+        name: '',
+        age: '',
+        email: ''
+    })
 
     useEffect(() => {
         axiosWithAuth()
@@ -13,14 +20,66 @@ const Friends = () => {
             })
             .catch(err => console.log(err));
     }, [])
+
+    const addFriend = (e) => {
+        e.preventDefault();
+        console.log(newFriend)
+
+        axiosWithAuth()
+            .post('http://localhost:5000/api/friends', newFriend)
+            .then(res => {
+                axiosWithAuth().get('http://localhost:5000/api/friends')
+                .then(res => {
+                    setFriendsList(res.data)
+                })
+                .catch(err => console.log(err))
+                console.log(res.data.payload);
+           
+            // props.history.push('/friendslist')
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    const handleChange = (e) => {
+        setNewFriend({ ...newFriend, [e.target.name]: e.target.value })
+    }
+
+    console.log('hi guys!')
     return (
         <div>
+
+            <form onSubmit={(e) => addFriend(e)}>
+                <p>Name:</p>
+                <input
+                    type='text'
+                    name='name'
+                    onChange={(e) => handleChange(e)}
+                />
+                <p>Age:</p>
+                <input
+                    type='text'
+                    name='age'
+                    onChange={(e) => handleChange(e)}
+                />
+                <p>Email:</p>
+                <input
+                    type='text'
+                    name='email'
+                    onChange={(e) => handleChange(e)}
+                />
+                <p></p>
+                <button>Add Friend</button>
+            </form>
             {friendsList.map((friend) => {
-                return (<div>
-                    <p>Name: {friend.name}</p>
-                    <p>Age: {friend.age}</p>
-                    <p>Email: {friend.email}</p>
-                </div>)
+                return (
+                    <div key={friend.id}>
+                        <p>Name: {friend.name}</p>
+                        <p>Age: {friend.age}</p>
+                        <p>Email: {friend.email}</p>
+                    </div>
+                )
             })}
         </div>
     )
